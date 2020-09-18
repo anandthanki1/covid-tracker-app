fd9c037a7d1a02e036195defbe025036bd2f6538
anandthanki1 <anandthanki18@gmail.com>
2020-09-17 23:21:18 -0700

    Add fetch api and worked on Cards component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
+import { fetchData } from './api';
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: {},
+        }
+    }
+
+    async componentDidMount() {
+        let fetchedData = await fetchData();
+        
+        this.setState({
+            data: fetchedData,
+        });
+    }
+
     render() {
+        const { data } = this.state;
         return (
             <div className={styles.container}>
                 <h1>Covid Tracker App</h1>
-                <Cards />
+                <Cards data={data} />
                 <Chart />
                 <CountryPicker />
             </div>
