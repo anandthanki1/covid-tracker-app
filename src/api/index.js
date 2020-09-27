7669f9bf063a71a363e9c252e84d7202b9193383
anandthanki1 <anandthanki18@gmail.com>
2020-09-26 19:07:50 -0700

    Added chart for infected and deaths count

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,10 @@
-import axios from 'axios';
-
 const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async () => {
     try {
 
-        let { data } = await axios.get(url);
+        let response = await fetch(url);
+        let data = await response.json();
 
         let modifiedData = {
             confirmed: data.confirmed,
@@ -21,4 +20,19 @@ export const fetchData = async () => {
     }
 };
 
-// export default fetchData;
+export const fetchDailyData = async () => {
+    try {
+        let response = await fetch(`${url}/daily`);
+        let data = await response.json();
+
+        const modifiedData = data.map((dailyData) => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate,
+        }));
+
+        return modifiedData;
+    } catch(error) {
+        console.log(error);
+    }
+}
